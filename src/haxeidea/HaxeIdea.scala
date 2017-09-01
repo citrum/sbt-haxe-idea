3cdaadd7bc035072752b964f8dd23a6840e4d66d
winmain <d.winmain@gmail.com>
2017-09-01 12:27:08 +0700

    Version 0.2.0. Totally revamped project. Now Idea modules not patched. Also, added haxe compiler java params.

--- src/haxeidea/HaxeIdea.scala
+++ src/haxeidea/HaxeIdea.scala
@@ -4,27 +4,21 @@ import sbt.Keys._
 import sbt._
 
 import scala.collection.mutable.ArrayBuffer
-import scala.xml.{Elem, Node, Text}
 
 object HaxeIdea {
-  // ------------------------------- Keys to define -------------------------------
-
-  val haxeIdeaModuleDir = settingKey[File]("Idea haxe module base directory. Example: 'app-js'")
-  val haxeIdeaModuleFile = settingKey[File]("Idea haxe module .iml file. Example: 'app-js/app-js.iml'")
-  val haxeIdeaSourceSubDirs = settingKey[Seq[String]]("Source directories inside base directory of idea haxe module. Example: Seq(\"src\")")
-
   // ------------------------------- Optional & internal keys -------------------------------
 
-  val haxeIdeaCp = taskKey[Seq[File]]("All haxe source directories (including dependencies). Just for haxe `-cp` parameters.")
-  val haxeIdeaManagedLibSubDirs = taskKey[Seq[File]]("All subdirectories in `haxeManagedLibSourceDir`. Internal task.")
-  val haxeIdeaManagedSubDir = settingKey[String]("Subdirectory of Idea haxe module to unpack haxe libraries source. Example: 'managed'.")
-  val haxeDependsOnIdeaModuleNames = settingKey[Seq[String]]("List of idea haxe module dependencies. Consists of idea module names.")
+  val managedHaxeSourceDirectory = settingKey[File]("Managed haxe source directory for unpacking dependencies.")
+
+  val managedHaxeSourceSubDirs = taskKey[Seq[File]]("All subdirectories in `managedHaxeSourceDirectory`. Internal task.")
+  val haxeCp = taskKey[Seq[File]]("All haxe source directories (including dependencies). Just for haxe `-cp` parameters.")
+
   val haxeDependsOnIdeaSourceDirs = settingKey[Seq[File]]("List of dependency idea haxe module source dirs. Internal usage when useRef flag is set.")
 
   val copyHaxeDepsFiles = taskKey[Seq[File]]("Unpack and copy dependent haxe libraries to `haxeManagedLibSourceDir`. Returns all copied files")
   val copyHaxeDepsInternal = taskKey[(Seq[File], Set[File])]("Unpack and copy dependent haxe libraries to `haxeManagedLibSourceDir`. Internal usage. Returns (directories, all_files)")
 
-  val haxeIdeaUpdate = taskKey[Unit]("Update .iml haxe module file")
+  val haxeCompilerJavaParams = taskKey[Seq[String]]("Parameters for haxe compiler in application ready to be sent via command line in `javaOptions`. For example: [\"-Dhaxe.cp=...\", \"-Dhaxe.bin=...\"]")
 
   // ------------------------------- RelativeHaxeProject -------------------------------
 
@@ -32,7 +26,7 @@ object HaxeIdea {
     file(System.getProperty("user.home") + "/ws/" + projectName)
   }
 
-  case class RelativeHaxeProject(name: String, moduleId: ModuleID, ideaModuleName: String, projectId: String = null, useRef: Boolean = false) {
+  case class RelativeHaxeProject(name: String, moduleId: ModuleID, projectId: String = null, useRef: Boolean = false) {
     if (useRef) println("Using haxe project reference for " + name)
 
     def projectFile: File = relativeHaxeProjectFile(name)
@@ -42,10 +36,12 @@ object HaxeIdea {
       else ProjectRef(projectFile, projectId)
 
     def dependsFor(proj: Project): Project =
-      if (useRef) proj.settings(
-        haxeDependsOnIdeaModuleNames += ideaModuleName,
-        haxeDependsOnIdeaSourceDirs ++= (sourceDirectories in Compile in project).value
-      )
+      if (useRef)
+        proj
+          .dependsOn(project)
+          .settings(haxeDependsOnIdeaSourceDirs ++=
+            (sourceDirectories in Compile in project).value ++
+              (unmanagedResourceDirectories in Compile in project).value)
       else proj.settings(libraryDependencies += moduleId)
   }
   implicit class _ProjectWrapper(proj: Project) {
@@ -54,30 +50,43 @@ object HaxeIdea {
 
   // ------------------------------- Tasks -------------------------------
 
-  lazy val haxeIdeaCpTask = haxeIdeaCp := {
-    haxeIdeaSourceSubDirs.value.map(haxeIdeaModuleDir.value / _) ++
-      haxeIdeaManagedLibSubDirs.value ++
-      haxeDependsOnIdeaSourceDirs.value
+  lazy val haxeCpTask = haxeCp := {
+    (haxeDependsOnIdeaSourceDirs.value ++
+      managedHaxeSourceSubDirs.value ++
+      (sourceDirectories in Compile).value
+      ).filter(_.exists()).distinct
   }
 
-  lazy val haxeManagedLibSubDirsTask = haxeIdeaManagedLibSubDirs := copyHaxeDepsInternal.value._1
+  lazy val managedHaxeSourceSubDirsTask = managedHaxeSourceSubDirs := copyHaxeDepsInternal.value._1
 
   /**
-    * Unpack and copy dependent haxe libraries to [[haxeIdeaManagedSubDir]]
+    * Unpack and copy dependent haxe libraries to [[managedHaxeSourceSubDirs]]
     */
   lazy val copyHaxeDepsInternalTask = copyHaxeDepsInternal := {
-    val managedSourceDir: File = haxeIdeaModuleDir.value / haxeIdeaManagedSubDir.value
+    val managedSourceDir: File = managedHaxeSourceDirectory.value
 
-    val classPath = (dependencyClasspath in Compile).value
+    val classPath: Classpath = (dependencyClasspath in Compile).value
     var unpackedFiles: Set[File] = Set.empty
     var unpackedDirs = new ArrayBuffer[File]()
-    for {attrFile <- classPath
+    for {attrFile <- classPath if !attrFile.data.isDirectory
          artifact <- attrFile.metadata.get(artifact.key) if artifact.classifier.exists(_ == "haxe")} {
+      val isHaxeJar: Boolean = artifact.name == "haxe-jar"
       val moduleId: ModuleID = attrFile.metadata.get(moduleID.key).get
       println("Unpacking haxe sources from module " + moduleId)
-      val unpackDir = new File(managedSourceDir, moduleId.organization + "@" + moduleId.name)
+      var unpackDir = new File(managedSourceDir, moduleId.organization + "@" + moduleId.name)
+
+      // Hack: standard library must be in directory named "std"
+      if (isHaxeJar) {
+        unpackDir = unpackDir / "std"
+      }
+
       unpackedDirs += unpackDir
       unpackedFiles ++= IO.unzip(attrFile.data, unpackDir)
+
+      // Small hack: after unpack make haxe binaries executable
+      if (isHaxeJar) {
+        (unpackDir / "bin").***.filter(!_.isDirectory).get.foreach(_.setExecutable(true))
+      }
     }
 
     // Delete all other files in managedSourceDir
@@ -106,67 +115,19 @@ object HaxeIdea {
 
   lazy val copyHaxeDepsFilesTask = copyHaxeDepsFiles := copyHaxeDepsInternal.value._2.toVector
 
-  /**
-    * Update .iml haxe module file
-    */
-  lazy val haxeIdeaUpdateTask = haxeIdeaUpdate := {
-    import _XmlUtils._
-    copyHaxeDepsFiles.value
-    val log = streams.value.log
-    val moduleDir: File = haxeIdeaModuleDir.value
-    val imlFile: File = haxeIdeaModuleFile.value
-    val subDirs: Seq[String] = haxeIdeaSourceSubDirs.value
-    val allLibDirs: Seq[File] = haxeIdeaManagedLibSubDirs.value
-    val dependsOnModules: Seq[String] = haxeDependsOnIdeaModuleNames.value
+  lazy val haxeCompilerJavaParamsTask = haxeCompilerJavaParams := {
+    val dirs: Seq[File] = haxeCp.value
+    val (haxeStdSeq: Seq[File], haxeCpDirs: Seq[File]) = dirs.partition(_ / "Std.hx" exists())
+    require(haxeStdSeq.nonEmpty, "No haxe standard library found. Add dependency to `haxe-jar` artifact.")
+    require(haxeStdSeq.size == 1, "More than one standard library defined. Choose only one `haxe-jar` artifact.")
+    val haxeStd: File = haxeStdSeq.head
+    val haxeBin: File = haxeStd / "bin/linux64/haxe"
+    require(haxeBin.exists(), "Cannot find haxe binary in " + haxeBin)
+    require(haxeBin.canExecute, "Haxe binary cannot be executable (no executable flag set?)")
 
-    val root: Elem = scala.xml.XML.loadFile(imlFile)
-    (root \ "component").find(hasAttrVal(_, "name", "NewModuleRootManager")).foreach {componentNode =>
-      var componentChilds: ArrayBuffer[Node] = componentNode.child.to
-
-      // Update local source dirs
-      val contentNode: Elem = (componentNode \ "content").head.asInstanceOf[Elem]
-      val newContentNode: Elem = {
-        val newChilds: ArrayBuffer[Node] = new ArrayBuffer[Node]()
-        for (subDir <- subDirs) {
-          newChilds += <sourceFolder isTestSource="false" url={"file://$MODULE_DIR$/" + subDir}/>
-        }
-
-        for (libDir <- allLibDirs) {
-          libDir.relativeTo(moduleDir) match {
-            case Some(relative) => newChilds += <sourceFolder isTestSource="false" url={"file://$MODULE_DIR$/" + relative}/>
-            case None => log.warn(s"Cannot libDir path '$libDir' not in module root dir '$moduleDir' so not included")
-          }
-        }
-
-        contentNode.copy(child = formatXml(newChilds, 6))
-      }
-      componentChilds(componentChilds.indexOf(contentNode)) = newContentNode
-
-      // Update dependency on modules
-      componentChilds = componentChilds.filterNot(n => n.label == "orderEntry" && hasAttrVal(n, "type", "module"))
-      for (depModule <- dependsOnModules) {
-        componentChilds += <orderEntry type="module" module-name={depModule}/>
-      }
-
-      // Replace xml childs
-      val newComponentNode = componentNode.asInstanceOf[Elem].copy(child = formatXml(componentChilds, 4))
-      val newRoot = root.asInstanceOf[Elem].copy(child = formatXml(root.child.filterNot(_ eq componentNode) :+ newComponentNode, 2))
-
-      // And save new .iml file
-      scala.xml.XML.save(imlFile.toString, newRoot, "utf-8")
-    }
-  }
-
-
-  private object _XmlUtils {
-    def hasAttrVal(n: Node, attrName: String, attrVal: String): Boolean =
-      n.attribute(attrName).exists(_.exists(_.text == attrVal))
-
-    def formatXml(nodes: Seq[Node], indent: Int): Seq[Node] = {
-      val indentStr: String = "\n" + (" " * indent)
-      nodes.filterNot(_.isInstanceOf[Text])
-        .flatMap(n => Seq(Text(indentStr), n)) ++ Text("\n" + (" " * (indent - 2)))
-    }
+    Seq("-Dhaxe.bin=" + haxeBin,
+      "-Dhaxe.std=" + haxeStd,
+      "-Dhaxe.cp=" + haxeCpDirs.mkString(":"))
   }
 
   // ------------------------------- Default settings -------------------------------
@@ -175,15 +136,14 @@ object HaxeIdea {
     * Default haxe project settings
     */
   lazy val haxeIdeaSettings = Seq[Setting[_]](
-    haxeIdeaManagedSubDir := "managed",
+    managedHaxeSourceDirectory := target.value / "managed-haxe",
 
-    haxeIdeaCpTask,
-    haxeManagedLibSubDirsTask,
+    haxeCpTask,
+    managedHaxeSourceSubDirsTask,
     copyHaxeDepsFilesTask,
     copyHaxeDepsInternalTask,
-    haxeIdeaUpdateTask,
+    haxeCompilerJavaParamsTask,
 
-    haxeDependsOnIdeaModuleNames := Nil,
     haxeDependsOnIdeaSourceDirs := Nil,
     resourceGenerators in Compile += copyHaxeDepsFiles.map(_ => Nil).taskValue // Return empty list for one purpose: unpacked and copied haxe files should not be included in final jar
   )
